d9385dcd8654d6a87cbab318799b8669735f9792
hynzu <150775196+hynzu@users.noreply.github.com>
2023-11-20 01:00:04 +0800

    Update question.py

--- question.py
+++ question.py
@@ -10,4 +10,11 @@ questions = [
     QuestionSet('What is the name of the Islamic charity given during the month of Ramadan?', ['Zakat', 'Zakt']),
     QuestionSet('In what city is the Kaaba located?', ['Mecca', 'Mekah']),
     QuestionSet('What is the first qibla for muslims?', ['Al-Aqsa Mosque', 'Masjid Al-Aqsa', 'Al-Aqsa', 'al aqsa']),
+    QuestionSet('What is the holy book of Islam?', ['Al-Quran', 'quran', 'Al quran', 'Quran', 'Al-quran']),
+    QuestionSet('What is the first pillar of Islam?', ['The shahada', 'Shahada', 'shahada', 'Declaration of faith']),
+    QuestionSet('How many names does Allah have?', ['99', 'Ninety-nine', 'ninety nine', 'ninety-nine']),
+    QuestionSet('Who was the first woman to embrace Islam?', ['Khadijah', 'Khadijah binti Khuwaylid', 'khadijah', 'khadijah bint khuwaylid']),
+    QuestionSet('Who is the last prophet of Islam?', ['Muhammad', 'muhammad', 'Prophet Muhammad', 'prophet muhammad']),
+    QuestionSet('How many times a day do Muslims pray?', ['5', 'five', 'Five']),
+    QuestionSet('What is the pilgrimage to Mecca called?', ['Hajj', 'hajj']),
 ]
